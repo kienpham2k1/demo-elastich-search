d4aae78e1934400e6be307cd42dd0cf7d481cc54
KienPT <kienpt.dev.01@gmail.com>
2025-05-31 17:51:50 +0700

    Initial commit

---
+++ src/main/java/org/example/demoelastichsearch/DemoElastichSearchApplication.java
@@ -0,0 +1,13 @@
+package org.example.demoelastichsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoElastichSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoElastichSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/demoelastichsearch/DemoElastichSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demoelastichsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoElastichSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
